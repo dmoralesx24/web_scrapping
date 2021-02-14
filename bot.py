cd51a465942f45a61845949258783c7175e00d6b
David <dmorales24@gmail.com>
2021-02-13 22:09:34 -0500

    first commit

---
+++ bot.py
@@ -0,0 +1,22 @@
+#TODO lIST FOR THIS BOT
+# sign into site with the product 
+# Find product for retail price of PS5 or Xbox Series X ($500)
+# If product isn't available, wait until it is.
+# Add product to the cart 
+# Add payment
+# Then checkout cart
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys 
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import ElementNotInteractableException, NoSuchElementException, StaleElementReferenceException
+from random import randict, randrange
+import time
+import random 
+
+website_url = "get url you're gonna use David"
+
+hello = "Hello World"
+print (hello) 
+myvar = ""
+
