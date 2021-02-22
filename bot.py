188b0446664a18dc78f4d3113e07f568282ddfc0
David Morales <dmoralesx24@gmail.com>
2021-02-22 14:29:48 -0500

    added a few more set up methods

--- bot.py
+++ bot.py
@@ -6,6 +6,8 @@
 # Add payment
 # Then checkout cart
 
+##change steps to fit specific website checkout routine 
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys 
 from selenium.webdriver.common.action_chains import ActionChains
@@ -15,8 +17,67 @@ import time
 import random 
 
 website_url = "get url you're gonna use David"
+test_url = ""
+waitTime = 7
 
 hello = "Hello World"
 print (hello) 
-myvar = ""
+
+class DavidShop:
+
+    def __init__ (self, username, password):
+        self.username = username
+        self.password = password
+        
+        self.driver = webdriver.Firefox()
+
+
+    def signIn(self):
+        driver = self.driver
+
+        ##enter username
+        username_elem = driver.find_element_by_xpath("//input[@name='email']")
+        username_elem.clear()
+        #possibly fake typing here
+        username_elem.send_keys(self.username)
+
+        time.sleep(randict(init(waitTime/2), waitTime))
+
+        username_elem.send_key(Keys.RETURN)
+
+        time.sleep(randict(init(waitTime/2), waitTime))
+
+        ## enter password
+        password_elem = driver.find_element_by_xpath("//input[@name='password']")
+        password_elem.clear()
+        #possibly fake typing here
+        password_elem.send_keys(self.password)
+
+        time.sleep(randict(init(waitTime/2), waitTime))
+
+        password_elem.send_key(Keys.RETURN)
+
+    def findProduct(self):
+        driver = self.driver
+        driver.get(website_url)
+        time.sleep(randict(init(waitTime/2), waitTime))
+
+        if self.isProductAvailable() == 'Currently unavailable':
+            time.sleep(randict(init(waitTime/2), waitTime))
+            self.findProduct()
+        else:
+            buy_now = driver.find_elemnt_by_name("but button")
+            buy_now.click()
+            time.sleep(randict(init(waitTime/2), waitTime))
+            self.signIn()
+
+            place_order = driver.find_element_by_name('place your order html tags')
+            time.sleep(randict(init(waitTime/2), waitTime))
+            place_order.click()
+            time.sleep(randict(init(waitTime/2), waitTime))
+
+    def isProductAvailable(self):
+        driver = self.driver
+        available = driver.find_element_by_class_name("find availabilty by class name")
+
 
