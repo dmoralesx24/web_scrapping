16a0efc06fb43b742273e99a9af7abe5421f208e
David Morales <dmoralesx24@gmail.com>
2021-02-22 22:26:42 -0500

    more set up code

--- bot.py
+++ bot.py
@@ -19,6 +19,7 @@ import random
 website_url = "get url you're gonna use David"
 test_url = ""
 waitTime = 7
+priceLimit = 500
 
 hello = "Hello World"
 print (hello) 
@@ -41,11 +42,11 @@ class DavidShop:
         #possibly fake typing here
         username_elem.send_keys(self.username)
 
-        time.sleep(randict(init(waitTime/2), waitTime))
+        time.sleep(randict(int(waitTime/2), waitTime))
 
         username_elem.send_key(Keys.RETURN)
 
-        time.sleep(randict(init(waitTime/2), waitTime))
+        time.sleep(randict(int(waitTime/2), waitTime))
 
         ## enter password
         password_elem = driver.find_element_by_xpath("//input[@name='password']")
@@ -53,31 +54,51 @@ class DavidShop:
         #possibly fake typing here
         password_elem.send_keys(self.password)
 
-        time.sleep(randict(init(waitTime/2), waitTime))
+        time.sleep(randict(int(waitTime/2), waitTime))
 
         password_elem.send_key(Keys.RETURN)
 
     def findProduct(self):
         driver = self.driver
         driver.get(website_url)
-        time.sleep(randict(init(waitTime/2), waitTime))
+        time.sleep(randict(int(waitTime/2), waitTime))
 
-        if self.isProductAvailable() == 'Currently unavailable':
-            time.sleep(randict(init(waitTime/2), waitTime))
+        isAvailable = self.isProductAvailable()
+        if isAvailable == 'Currently unavailable':
+            time.sleep(randict(int(waitTime/2), waitTime))
             self.findProduct()
-        else:
+        elif isAvailable <= priceLimit:
             buy_now = driver.find_elemnt_by_name("but button")
             buy_now.click()
-            time.sleep(randict(init(waitTime/2), waitTime))
+            time.sleep(randict(int(waitTime/2), waitTime))
             self.signIn()
 
             place_order = driver.find_element_by_name('place your order html tags')
-            time.sleep(randict(init(waitTime/2), waitTime))
+            time.sleep(randict(int(waitTime/2), waitTime))
             place_order.click()
-            time.sleep(randict(init(waitTime/2), waitTime))
+            time.sleep(randict(int(waitTime/2), waitTime))
+        else:
+            time.sleep(randict(int(waitTime/2), waitTime))
+            self.findProduct()
 
     def isProductAvailable(self):
         driver = self.driver
         available = driver.find_element_by_class_name("find availabilty by class name")
+        if available == 'Currently Unavailable':
+            print(f'AVAILABLE: {available}')
+            return available
+        else:
+            print(f'PRICE: {available}')
+            return float(available[1:])
+
+    def closeBrowser(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    shopBot = DavidShop("username", "password")
+    shopBot.findProduct()
+    shopBot.closeBrowser()
+
+
 
 
