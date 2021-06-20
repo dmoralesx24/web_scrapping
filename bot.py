09eb2ff7689adb974cbf63f70f56d2edf05039ef
David <dmorales24@gmail.com>
2021-06-19 22:58:01 -0400

    finally have a working app. going to add details for scrapping later and eventually add a UI

--- bot.py
+++ bot.py
@@ -8,30 +8,50 @@
 
 ##change steps to fit specific website checkout routine 
 
+##POSSIBLE WEBSITES TO USE FOR PS5
+##////////////////////////
+#Walmart: PS5 | PS5 Digital Edition (last restock June 17)
+#GameStop: PS5 | PS5 Digital Edition (last restock June 18)
+#Target: PS5 | PS5 Digital Edition (last restock June 16)
+#Sony: PS5 | PS5 Digital Edition (last restock June 17)
+##////////////////////////
+
+##POSSIBLE WEBSITES TO USE FOR Xbox Series X
+##////////////////////////
+#Walmart: PS5 | PS5 Digital Edition (last restock June 17)
+#GameStop: PS5 | PS5 Digital Edition (last restock June 18)
+#Target: PS5 | PS5 Digital Edition (last restock June 16)
+#Microsoft: Xbox Series X | Xbox Series S (last restock June 8)
+##////////////////////////
+
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys 
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import ElementNotInteractableException, NoSuchElementException, StaleElementReferenceException
-from random import randict, randrange
+from random import randint, randrange
 import time
-import random 
 
 website_url = "get url you're gonna use David"
 test_url = ""
 waitTime = 7
 priceLimit = 500
 
-hello = "Hello World"
-print (hello) 
-
 class DavidShop:
 
     def __init__ (self, username, password):
         self.username = username
         self.password = password
-        
-        self.driver = webdriver.Firefox()
-
+        self.options = Options()
+        self.binary = r'../../Program Files/Mozilla Firefox/firefox.exe'
+        self.options.set_preference("browser.download.folderList",2)
+        self.options.set_preference("browser.download.manager.showWhenStarting", False)
+        self.options.set_preference("browser.download.dir","/Data")
+        self.options.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/octet-stream,application/vnd.ms-excel")
+        self.options.binary = self.binary
+        self.options.binary_location = r'C:\Program Files\Mozilla Firefox\firefox.exe'
+        self.driver = webdriver.Firefox(executable_path=r'./geckodriver.exe', options=self.options)
 
     def signIn(self):
         driver = self.driver
@@ -42,11 +62,11 @@ class DavidShop:
         #possibly fake typing here
         username_elem.send_keys(self.username)
 
-        time.sleep(randict(int(waitTime/2), waitTime))
+        time.sleep(randint(int(waitTime/2), waitTime))
 
         username_elem.send_key(Keys.RETURN)
 
-        time.sleep(randict(int(waitTime/2), waitTime))
+        time.sleep(randint(int(waitTime/2), waitTime))
 
         ## enter password
         password_elem = driver.find_element_by_xpath("//input[@name='password']")
@@ -54,31 +74,31 @@ class DavidShop:
         #possibly fake typing here
         password_elem.send_keys(self.password)
 
-        time.sleep(randict(int(waitTime/2), waitTime))
+        time.sleep(randint(int(waitTime/2), waitTime))
 
         password_elem.send_key(Keys.RETURN)
 
     def findProduct(self):
         driver = self.driver
         driver.get(website_url)
-        time.sleep(randict(int(waitTime/2), waitTime))
+        time.sleep(randint(int(waitTime/2), waitTime))
 
         isAvailable = self.isProductAvailable()
         if isAvailable == 'Currently unavailable':
-            time.sleep(randict(int(waitTime/2), waitTime))
+            time.sleep(randint(int(waitTime/2), waitTime))
             self.findProduct()
         elif isAvailable <= priceLimit:
             buy_now = driver.find_elemnt_by_name("but button")
             buy_now.click()
-            time.sleep(randict(int(waitTime/2), waitTime))
+            time.sleep(randint(int(waitTime/2), waitTime))
             self.signIn()
 
             place_order = driver.find_element_by_name('place your order html tags')
-            time.sleep(randict(int(waitTime/2), waitTime))
+            time.sleep(randint(int(waitTime/2), waitTime))
             place_order.click()
-            time.sleep(randict(int(waitTime/2), waitTime))
+            time.sleep(randint(int(waitTime/2), waitTime))
         else:
-            time.sleep(randict(int(waitTime/2), waitTime))
+            time.sleep(randint(int(waitTime/2), waitTime))
             self.findProduct()
 
     def isProductAvailable(self):
