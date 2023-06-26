8be08d64e437d3d4dcc6a15da3021f1ac0e4508e
David Morales <dmoralesx24@gmail.com>
2023-06-26 19:30:36 -0400

    some formatting cleanup

--- bot.py
+++ bot.py
@@ -1,71 +1,68 @@
-#TODO lIST FOR THIS BOT
-# sign into site with the product 
+
+# TODO lIST FOR THIS BOT
+# sign into site with the product
 # Find product for retail price of PS5 or Xbox Series X ($500)
 # If product isn't available, wait until it is.
-# Add product to the cart 
+# Add product to the cart
 # Add payment
 # Then checkout cart
 
-##change steps to fit specific website checkout routine 
+# change steps to fit specific website checkout routine
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys 
+from selenium.webdriver.common.keys import Keys
 # from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.common.exceptions import ElementNotInteractableException, NoSuchElementException, StaleElementReferenceException
+# from selenium.webdriver.common.action_chains import ActionChains
+# from selenium.common.exceptions import ElementNotInteractableException, \
+# NoSuchElementException, StaleElementReferenceException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from random import randint, randrange
+from random import randint
 import time
 from dotenv import dotenv_values
 
-#REGION PS5 links
-amazon_url = 'https://www.amazon.com/Sony-Playstation-Disk/dp/B082S2LYV1/ref=sr_1_3?crid=NQFUKMT8FF9R&dchild=1&keywords=playstation+5+console&qid=1635369528&sprefix=plays%2Caps%2C136&sr=8-3'
-walmart_url = 'https://www.walmart.com/ip/Sony-PlayStation-5-Video-Game-Console/165545420'
-target_url = 'https://www.target.com/p/playstation-5-console/-/A-81114595?clkid=cf3af1bdN584011ebb49742010a246e3a&lnm=81938&afid=Future%20PLC.&ref=tgt_adv_xasd0002'
-bestbuy_url = 'https://www.bestbuy.com/site/sony-playstation-5-console/6426149.p?skuId=6426149'
-bestbuy_test_url = 'https://www.bestbuy.com/site/asus-11-6-chromebook-intel-celeron-4gb-memory-32gb-emmc-flash-memory-gray-grey/6449513.p?skuId=6449513'
-#END REGION
+# REGION PS5 links
+amazon_url = 'https://www.amazon.com/Sony-Playstation-Disk/dp/B082S2LYV1/\
+ref=sr_1_3?crid=NQFUKMT8FF9R&dchild=1&keywords=playstation+5+console&qid=1635369528&\
+sprefix=plays%2Caps%2C136&sr=8-3'
+walmart_url = 'https://www.walmart.com/ip/\
+Sony-PlayStation-5-Video-Game-Console/165545420'
+target_url = 'https://www.target.com/p/playstation-5-console/-/A-81114595?\
+clkid=cf3af1bdN584011ebb49742010a246e3a&lnm=81938&afid=Future%20PLC.&\
+ref=tgt_adv_xasd0002'
+bestbuy_url = 'https://www.bestbuy.com/site/sony-playstation-5-console\
+/6426149.p?skuId=6426149'
+# END REGION
 
-website_url = bestbuy_test_url
+website_url = amazon_url
 waitTime = 7
 priceLimit = 500
 
+
 class DavidShop:
 
-    def __init__ (self, username, password):
+    def __init__(self, username, password):
         self.username = username
         self.password = password
         self.options = Options()
-        
-        # self.options.add_argument("./chrome.exe")
         self.options.add_argument("--window-size=1920x1080")
         self.options.add_argument("--verbose")
-        # self.options.add_argument('headless')
-        self.options.add_argument('--disable-infobars')
-        self.options.add_argument('--disable-dev-shm-usage')
-        self.options.add_argument('--no-sandbox')
-        self.options.add_argument('--remote-debugging-port=9222')
-        # TODO: need to create a seperate binary location based on operating system running. one for macOS another for WSL.
-        # TODO: also need to make sure chromedriver.exe is updated. may need a different driver for macOS.
-        #
-        # self.options.binary_location = 'C:\Program Files\Google\Chrome\Application\chrome.exe'
-        self.options.binary_location = '/mnt/c/Program Files/Google/Chrome/Application/./chrome.exe'
-        # self.options.binary_location = '/usr/bin/google-chrome /usr/share/man/man1/google-chrome.1.gz'
-        # self.options.
-        # self.driver = webdriver.Firefox(executable_path=r'./geckodriver.exe', options=self.options)
-        self.driver = webdriver.Chrome(options=self.options)
 
+        # self.driver = webdriver.Firefox(
+        # executable_path=r'./geckodriver.exe',
+        # options=self.options
+        # )
+        self.driver = webdriver.Chrome(options=self.options)
 
     def signIn(self):
         driver = self.driver
-        ##enter username
+        # enter username
         username_elem = driver.find_element_by_xpath("//input[@name='email']")
 
         username_elem.clear()
-        #possibly fake typing here
+        # possibly fake typing here
         username_elem.send_keys(self.username)
 
         # time.sleep(randint(int(waitTime/1), waitTime))
@@ -74,15 +71,18 @@ class DavidShop:
 
         time.sleep(randint(int(waitTime/1), waitTime))
 
-
-        ## enter password
-        # password_elem = self.webDriverWait(1, 'xpath', "//input[@name='password']")
-        password_elem = driver.find_element_by_xpath("//input[@name='password']")
+        # enter password
+        # password_elem = self.webDriverWait(
+        # 1,
+        # 'xpath',
+        # "//input[@name='password']"
+        # )
+        password_elem = driver.find_element_by_xpath(
+            "//input[@name='password']")
 
         password_elem.clear()
-        #possibly fake typing here
+        # possibly fake typing here
         password_elem.send_keys(self.password)
-
         # time.sleep(randint(int(waitTime/1), waitTime))
 
         password_elem.send_keys(Keys.ENTER)
@@ -91,43 +91,40 @@ class DavidShop:
         driver = self.driver
         driver.get(website_url)
         time.sleep(randint(int(waitTime/1), waitTime))
-        
-        # NOTE: checks if buy button is available
-        btn = self.isProductAvailable("add-to-cart-button")
-        print(btn)
+
+        btn = self.isProductAvailable()
         if btn == 'Currently Unavailable':
             # time.sleep(randint(int(waitTime/1), waitTime))
             self.findProduct()
-         
-        btn.click()
-        time.sleep(randint(int(waitTime/1), waitTime))
-        
-        # NOTE: Looks for cart button    todo: figure out an exit strategy if it doesn't find the button
-        btn2 = self.isProductAvailable("go-to-cart-button")
-        btn2.click()
-        # self.signIn()
+        else:
+            btn.click()
+            time.sleep(randint(int(waitTime/1), waitTime))
 
-        time.sleep(randint(int(waitTime/1), waitTime))
+            self.signIn()
 
-        place_order = driver.find_element_by_name('place your order html tags')
-        place_order.click()
-        time.sleep(randint(int(waitTime/1), waitTime))
-        
+            time.sleep(randint(int(waitTime/1), waitTime))
 
-    def isProductAvailable(self, tagname):
+            place_order = driver.find_element_by_name(
+                'place your order html tags')
+            place_order.click()
+            time.sleep(randint(int(waitTime/1), waitTime))
+
+    def isProductAvailable(self):
         driver = self.driver
         btn = None
         try:
             btn = WebDriverWait(driver, 1).until(
-                EC.presence_of_element_located((By.CLASS_NAME, tagname))
+                EC.presence_of_element_located((By.ID, "buy-now-button"))
             )
-        except:
+        except Exception as e:
+            print(e)
             btn = 'Currently Unavailable'
-        
         return btn
-        
+
     def webDriverWait(self, time, id, name):
-        enumElement = {'id': By.ID, 'classname': By.CLASS_NAME, 'xpath': By.XPATH}
+        enumElement = {
+            'id': By.ID, 'classname': By.CLASS_NAME, 'xpath': By.XPATH
+        }
         driver = self.driver
         res = {'found': False}
         try:
@@ -135,21 +132,17 @@ class DavidShop:
                 EC.presence_of_element_located((enumElement[id], name)))
             res['found'] = True
             res['element'] = element
-        except:
+        except Exception as e:
             print('There has been an error thrown...couldnt find element')
-        
+            print(e)
         return res
 
     def closeBrowser(self):
         self.driver.close()
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     config = dotenv_values('.env')
     shopBot = DavidShop(config['AMAZON_USERNAME'], config['AMAZON_PASSWORD'])
     shopBot.findProduct()
     shopBot.closeBrowser()
-
-
-
-
